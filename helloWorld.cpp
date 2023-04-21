8d4e5a737ef69036309736759e1e2e5389eca1b9
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-21 19:27:32 +0300

    Added my first C++ program

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
+
+    // this is a comment
+
+    /*
+        This
+        is 
+        a 
+        multi-line
+        comment
+    */
+
     std::cout << "I like pizza!" << std::endl;
     std::cout << "It's really good!" << std::endl;
     return 0;
