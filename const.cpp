32503e86e1f6f417dadba6000c831ba8a43999ec
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-22 15:54:08 +0300

    Const keyword

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792358;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+    
+    return 0;
+}
