feea97f1d0e6cf7b11c5f7a7d68512e53d5920c5
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-22 16:43:30 +0300

    Arithmetic operators part

---
+++ arithmeticOperators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    int students = 20;
+    // students = students + 1;
+    // students += 2;
+    // students++;
+
+    // students = students - 1;
+    // students -= 1;
+    // students--;
+
+    // students = students * 2;
+    // students  *= 2;
+
+    // students = students / 2;
+    // students /= 2;
+
+    // students = students % 2;
+    // students %= 2; 
+
+    std::cout << students;
+
+    return 0;
+}
