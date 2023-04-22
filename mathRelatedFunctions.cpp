cc2a9acf512f18a7b235d90fcd9e5c353d8fb1df
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-22 17:15:55 +0300

    Math related fuctions part

---
+++ mathRelatedFunctions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double x = 3;
+    double y = 4;
+    double z;
+
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(x, y);
+    // z = sqrt(9);
+    // z = abs(-10);
+    // z = round(3.14);
+    // z = ceil(3.14);
+    z = floor(3.14);
+
+    std::cout << z;
+    return 0;
+}
