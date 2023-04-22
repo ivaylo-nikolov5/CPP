120d8b112fd27a3a1ba14ce55d0cc9169c70a87a
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-22 16:51:55 +0300

    Type conversion part

---
+++ typeConversion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // int x = 3.14;
+
+    // Implicit = automatic conversion
+    // std::cout << x << "\n";
+
+    // // Explicit Precede value with new data types
+    // std::cout << (int) 3.14;
+
+    // ---------------------------------------
+
+    char x = 100;
+    int y = 100;
+
+    std::cout << x << "\n";
+    std::cout << (char) y;
+
+    return 0;
+}
