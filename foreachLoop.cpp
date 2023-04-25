b2011a1b2d3d08ddcd0bb44fa745d0c9b745e219
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-04-25 17:42:15 +0300

    Foreach loop

---
+++ foreachLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
+    
+    // for (std::string student : students) {
+    //     std::cout << student << "\n";
+    // }   
+
+    int grades[] = {2, 3, 4, 5, 6};
+
+    for (int grade : grades) {
+        std::cout << grade << "\n";
+    }
+
+    return 0;
+}
